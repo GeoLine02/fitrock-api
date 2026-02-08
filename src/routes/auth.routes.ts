09c48ee67477bf8d3aa4193fc6e146bed591c224
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-02-08 15:45:37 +0400

    feat: create refresh token service

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import exprss from "express";
 import {
   loginUserController,
   logOutUserController,
+  refreshTokenController,
   registerUserController,
 } from "../controllers/auth.controller";
 import { validate } from "../middleware/requestValidator";
@@ -11,5 +12,6 @@ const router = exprss.Router();
 
 router.post("/register", validate(registerUserSchema), registerUserController);
 router.post("/login", validate(loginUserSchema), loginUserController);
+router.post("/refresh-token", refreshTokenController);
 router.post("/logout", logOutUserController);
 export default router;
