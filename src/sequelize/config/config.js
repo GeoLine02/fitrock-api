4536e5059cf8a8c26259ca625fccbd9fc74ce790
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-02-09 16:53:05 +0400

    hotfix: production variables structure

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -10,23 +10,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    use_env_variable: "INTENRAL_DB_URL", // Render provides this
     dialect: "postgres",
     logging: false,
-
-    production: {
-      use_env_variable: "INTENRAL_DB_URL", // Render provides this
-      dialect: "postgres",
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, // needed for some hosted DBs
-        },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // needed for some hosted DBs
       },
     },
   },
